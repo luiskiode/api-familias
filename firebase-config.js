676259c8eedc38acfc75ea770c188bd0878f23f5
luiskiode <luissj002@gmail.com>
2025-08-05 16:02:18 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyD5X6LUG_dOwuaUf6jQUHw_LM5PFgOmc40",
+  authDomain: "web-familias.firebaseapp.com",
+  projectId: "web-familias",
+  storageBucket: "web-familias.firebasestorage.app",
+  messagingSenderId: "261699620792",
+  appId: "1:261699620792:web:8ff59afb72e96c371dc94d"
+};
