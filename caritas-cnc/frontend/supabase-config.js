f49e704f78c7324ec6c234ae0eb3189867cae40d
luiskiode <luissj002@gmail.com>
2025-08-12 12:17:57 -0500

    Add files via upload

--- caritas-cnc/frontend/supabase-config.js
+++ caritas-cnc/frontend/supabase-config.js
@@ -1,3 +1,6 @@
 // supabase-config.js
+
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const supabaseUrl = "https://qivjlsvcjyqymommfdke.supabase.co";
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpdmpsc3ZjanlxeW1vbW1mZGtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNjA2MjUsImV4cCI6MjA2OTkzNjYyNX0.YN0W5miJeWwu96jhqKiGB-cA8t9TeIboQWURCjTFVbw";
